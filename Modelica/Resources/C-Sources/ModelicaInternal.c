e5ff56e918cc64e8dbc3bbb134769cb66fe4ce2b
Markus Olsson <markus.olsson@modelon.com>
2022-08-09 12:49:20 +0200

    Applied ModelicaInternal_fullPathName fix

--- Modelica/Resources/C-Sources/ModelicaInternal.c
+++ Modelica/Resources/C-Sources/ModelicaInternal.c
@@ -688,7 +688,8 @@ _Ret_z_ const char* ModelicaInternal_fullPathName(_In_z_ const char* name) {
             name, strerror(errno));
         return "";
     }
-    fullName = ModelicaDuplicateString(tempName);
+    fullName = ModelicaAllocateString(strlen(tempName));
+    strcpy(fullName, tempName);
     ModelicaConvertToUnixDirectorySeparator(fullName);
     return fullName;
 #elif defined(MODELICA_INTERNAL_HAVE_POSIX_REALPATH)
@@ -700,7 +701,8 @@ _Ret_z_ const char* ModelicaInternal_fullPathName(_In_z_ const char* name) {
     if (tempName == NULL) {
         goto FALLBACK_getcwd;
     }
-    fullName = ModelicaDuplicateString(tempName);
+    fullName = ModelicaAllocateString(strlen(tempName) + 1);
+    strcpy(fullName, tempName);
     ModelicaConvertToUnixDirectorySeparator(fullName);
     /* Retain trailing slash to match _fullpath behaviour */
     len = strlen(name);
